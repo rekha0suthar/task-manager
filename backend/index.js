92d64d3e9b11f6b9e1aed59e71df1ca0018cd1f4
REKHA SUTHAR <rekha0suthar@gmail.com>
2024-11-06 14:16:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,7 @@ app.use('/api/todos', todoRoutes);
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
