97132ea81de7a08e71854f413caabb30daa43081
rekha0suthar <rekha0suthar@gmail.com>
2024-12-11 15:17:30 +0530

    api routes

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => `MongoDB connect`)
